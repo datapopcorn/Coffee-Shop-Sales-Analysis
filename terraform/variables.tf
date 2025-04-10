d213f152d4de7dda452f56b04765d6c4ba708c31
datapopcorn <yanchengdatalife@gmail.com>
2025-04-10 11:33:51 +0800

    add terraform for gcs bucket and bigquery dataset

---
+++ terraform/variables.tf
@@ -0,0 +1,41 @@
+variable "credentials" {
+  description = "My Credentials"
+  default     = "./keys/my_creds.json"
+  #ex: if you have a directory where this file is called keys with your service account json file
+  #saved there as my-creds.json you could use default = "./keys/my-creds.json"
+}
+
+
+variable "project" {
+  description = "Project"
+  default     = "cloud-385312"
+}
+
+variable "region" {
+  description = "Region"
+  #Update the below to your desired region
+  default     = "asia-east1"
+}
+
+variable "location" {
+  description = "Project Location"
+  #Update the below to your desired location
+  default     = "asia-east1"
+}
+
+variable "bq_dataset_name" {
+  description = "My BigQuery Dataset Name"
+  #Update the below to what you want your dataset to be called
+  default     = "coffee_shop_analysis"
+}
+
+variable "gcs_bucket_name" {
+  description = "My Storage Bucket Name"
+  #Update the below to a unique bucket name
+  default     = "coffee_shop_analysis_bucket"
+}
+
+variable "gcs_storage_class" {
+  description = "Bucket Storage Class"
+  default     = "STANDARD"
+}
