5b8b2de1c23d5bfac70adfff18cd4147e980116f
datapopcorn <yanchengdatalife@gmail.com>
2025-04-20 01:10:52 +0800

    add incremental and partition in fct_transactions model

--- elt-pipeline/airflow/dbt/coffee_shop_sales_analysis/models/cafe/marts/fct_transactions.sql
+++ elt-pipeline/airflow/dbt/coffee_shop_sales_analysis/models/cafe/marts/fct_transactions.sql
@@ -1,5 +1,21 @@
+# config incremental strategy
+{{
+    config(
+        materialized='incremental',
+        incremental_strategy='merge',
+        unique_key='transaction_id',
+        partition_by={
+            'field': 'transaction_date',
+            'data_type': 'date'
+        },
+        on_schema_change='fail'
+    )
+}}
 with transactions as (
     select * from {{ ref('stg_transactions') }}
+    {% if is_incremental() %}
+    where date(created_at) >= (select max(transaction_date) from {{ this }})
+    {% endif %}
 ),
 transactions_static as (
     select * from {{ ref('stg_transactions_static') }}
@@ -15,6 +31,8 @@ transactions_join_payment_methods as (
         transactions.payment_method_id,
         transactions.total_spent,
         transactions.location,
+        transactions.created_at,
+        transactions.updated_at,
         date(transactions.created_at) as transaction_date,
         payment_methods.name as payment_method_name
     from transactions
@@ -28,6 +46,8 @@ transactions_union_static as (
         payment_method_name,
         location,
         total_spent,
+        created_at,
+        updated_at,
         transaction_date
     from transactions_join_payment_methods
     union all
@@ -37,6 +57,8 @@ transactions_union_static as (
         payment_method as payment_method_name,
         location,
         total_spent,
+        null as created_at,
+        null as updated_at,
         transaction_date
     from transactions_static
 )
